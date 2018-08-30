3a32e095d0e2574a6086a8ff25f9a2c78966f5e3
anderson <anderson@ferneda.com.br>
2018-08-30 20:44:32 -0300

    update

--- semana1/login-simples.py
+++ semana1/login-simples.py
@@ -4,5 +4,12 @@
 29/08/2018 - 22:35
 por Anderson Ferneda'''
 
+''' O que fiz
 user = raw_input("username:")
 print "Seja Bem Vindo %s " % user
+O que foi feito'''
+
+login = raw_input("Digite o seu login de usuario: ")
+senha = raw_input("Digite a sua senha: ")
+
+print "Seja Bem Vindo %s!" % login
--- semana1/login.py
+++ semana1/login.py
@@ -1,8 +1,21 @@
 #!/usr/bin/python
 
-user = raw_input("username:")
-senha = raw_input("senha:")
+''' Como fiz
+user = raw_input("Digite o seu login de usuario: ")
+senha = raw_input("Digite a sua senha: ")
 if user == "arthur.dent" and senha == "mochileiro":
     print "Usuario autenticado com sucesso!"
 else:
-    print "Acesso da nega:0"
+    print "Acesso da nega:0"'''
+
+usuario = "arthur.dent"
+password = "mochileiro"
+
+login = raw_input("Digite o seu login de usuario: ")
+senha = raw_input("Digite a sua senha: ")
+
+if (login == usuario) and (senha == password):
+    print "Usuario autenticado com sucesso!"
+    print "Seja Bem Vindo %s" % usuario
+else:
+    print "Acesso da negado!"
