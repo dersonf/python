0e3833256d34af03edd8473ed3c9929a6af3cd80
anderson <anderson@ferneda.com.br>
2018-08-30 21:36:22 -0300

    aula 2

---
+++ semana1/Aula 2/lista.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+
+grupos ["alta audiencia","ecommerce","oper conteudo","plataforma","email"]
+
+print (grupos)
---
+++ semana1/Aula 2/sistema.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python
+
+login = raw_input("Digite seu usuario de sistema: ")
+senha = raw_input("Digite sua senha: ")
+
+print "Seja Bem Vindo %s." % login
